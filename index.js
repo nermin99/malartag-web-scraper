11c4335063d7fe0eddb414e133039a1d112cb8b1
Nermin Skenderovic <nermin@skenderovic.se>
2023-05-01 11:07:05 +0200

    save results as array of objects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio')
 const puppeteer = require('puppeteer')
 
 // const SITE_URL =
-;('https://www.trafikverket.se/trafikinformation/tag/?Station=Stockholm%20C&ArrDep=departure')
+//   'https://www.trafikverket.se/trafikinformation/tag/?Station=Stockholm%20C&ArrDep=departure'
 
 const SITE_URL =
   'https://www.trafikverket.se/trafikinformation/tag?Station=Nykvarn&ArrDep=departure'
@@ -21,12 +21,18 @@ const start = async () => {
   // .time-advertised-time .time-strikethrough
   const $trs = $('tr td .time-strikethrough').parent().parent()
 
+  const results = []
   for (const tr of $trs) {
-    console.log($(tr).find('.time-strikethrough').text())
-    console.log($(tr).find('.format-estimated-time').text())
-    console.log($(tr).find('.station-name').text())
-    console.log($(tr).find('.cancelled').length > 0)
+    results.push({
+      time: $(tr).find('.time-strikethrough').text(),
+      estimatedTime: $(tr).find('.format-estimated-time').text(),
+      cancelled: $(tr).find('.cancelled').length > 0,
+      stationName: $(tr).find('.station-name').text(),
+      trainNr: $(tr).find('.train').text(),
+    })
   }
+
+  console.log(results)
 
   browser.close()
 }
