57ac6a5dd27f97c7ed733a9ee9ff5c64de52dac8
Nermin Skenderovic <nermin@skenderovic.se>
2023-05-01 12:02:37 +0200

    Add aws-sdk

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 const cheerio = require('cheerio')
 const puppeteer = require('puppeteer')
+const AWS = require('aws-sdk')
+require('dotenv').config()
+
+AWS.config.update({
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient()
+const TABLE_NAME = 'malartag-scraper'
+
+// const getCharacters = async () => {
+//   const params = {
+//     TableName: TABLE_NAME,
+//   }
+//   const characters = await dynamoClient.scan(params).promise()
+//   console.log(characters)
+//   return characters
+// }
+
+// getCharacterById = async (id) => {
+//   const params = {
+//     TableName: TABLE_NAME,
+//     Key: {
+//       id,
+//     },
+//   }
+//   result = await dynamoClient.get(params).promise()
+//   console.log(result)
+//   return result
+// }
+
+// const addOrUpdateCharacter = async (character) => {
+//   const params = {
+//     TableName: TABLE_NAME,
+//     Item: character,
+//   }
+//   const result = await dynamoClient.put(params).promise()
+//   console.log(result)
+//   return result
+// }
+// const hp = {
+//   id: '0',
+//   name: 'Harry Potter',
+//   house: 'Gryffindor',
+// }
 
 // const SITE_URL =
 //   'https://www.trafikverket.se/trafikinformation/tag/?Station=Stockholm%20C&ArrDep=departure'
