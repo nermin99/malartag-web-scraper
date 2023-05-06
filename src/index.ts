490d5c738d8e13adf95df651f863ccb1538c190b
Nermin Skenderovic <nermin@skenderovic.se>
2023-05-06 13:34:20 +0200

    BIG overhaul, this is actually supposed to work but waitUntil doesn't work with AWS

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import cheerio from 'cheerio'
-import puppeteer from 'puppeteer-core'
 import chromium from 'chrome-aws-lambda'
+import cheerio from 'cheerio'
 
 // const SITE_URL =
 //   'https://www.trafikverket.se/trafikinformation/tag/?Station=Stockholm%20C&ArrDep=departure'
 const SITE_URL =
   'https://www.trafikverket.se/trafikinformation/tag?Station=Nykvarn&ArrDep=departure'
 
-export const handler = async (event) => {
+export async function handler(event) {
+  await run()
+
+  const response = {
+    statusCode: 200,
+    body: '',
+  }
+
+  return response
+}
+
+export async function run() {
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
+    ignoreHTTPSErrors: true,
   })
   const page = await browser.newPage()
 
-  await page.goto(SITE_URL, { waitUntil: 'networkidle2' })
-
-  await page.setViewport({ width: 1080, height: 1024 })
+  await page.goto(SITE_URL, { waitUntil: 'networkidle2', timeout: 60000 })
 
   const html = await page.content()
   const $ = cheerio.load(html)
   const $trs = $('tr td .time-strikethrough').parent().parent()
 
-  const results = []
+  const results: any = []
   for (const tr of $trs) {
     results.push({
       time: $(tr).find('.time-strikethrough').text(),
@@ -39,5 +48,3 @@ export const handler = async (event) => {
 
   browser.close()
 }
-
-handler() // TODO: REMOVE ME IN AWS
